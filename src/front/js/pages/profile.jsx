8b37aeed16c79eaa70ed42661667faf3d0085ebc
pablodevs <pabloalamovargas@gmail.com>
2022-02-18 20:04:48 +0000

    images random working

--- src/front/js/pages/profile.jsx
+++ src/front/js/pages/profile.jsx
@@ -1,25 +1,32 @@
-import React, { useContext } from "react";
-import CloudinaryUploadWidget from "../component/cloudinary";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 
 export const Profile = () => {
     const { store, actions } = useContext(Context);
 
+    useEffect(() => {
+        actions.getImagesByTag("gusinet");
+    }, []);
+
     return (
         <div className="profile center flex-col">
             <h1 className="profile__title">Profile</h1>
             <p>Click on the image to change it!</p>
-            <div className="flex">
-                <CloudinaryUploadWidget preset="gusinet" />
-                {/* <CloudinaryUploadWidget preset="gusinette" /> */}
-            </div>
-            <img
-                src="https://res.cloudinary.com/peibol888/image/upload/v1645200689/gusinettes/gusinet/vofcwzrienu8rxak5q8i.jpg"
-                // src="https://res.cloudinary.com/peibol888/image/upload/v1645200648/gusinettes/gusinet/xvsnklzl4hakgkduog2f.jpg"
-                width="300"
-                alt="random profile image"
-                className="profile_img"
-            />
+            <button
+                className="btn btn--primary"
+                onClick={() => actions.getRandomImage()}>
+                Random Image
+            </button>
+            {store.randomImage ? (
+                <img
+                    src={store.randomImage}
+                    width="300"
+                    alt="random profile image"
+                    className="profile_img"
+                />
+            ) : (
+                "LOADING..."
+            )}
         </div>
     );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -8,6 +8,27 @@ const getState = ({ getStore, getActions, setStore }) => {
                 localStorage.setItem("end", true);
                 setStore({ counterEnd: true });
             },
+
+            getImagesByTag: tag => {
+                fetch(`${process.env.BACKEND_URL}/api/images/${tag}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        let actions = getActions();
+                        setStore({ images: data });
+                        actions.getRandomImage();
+                    })
+                    .catch(error => console.error(error));
+            },
+
+            getRandomImage: () => {
+                let store = getStore();
+                setStore({
+                    randomImage:
+                        store.images[
+                            Math.floor(Math.random() * store.images.length)
+                        ],
+                });
+            },
         },
     };
 };
