97dd2221badc032b28a1dd8cbee47262b5b89011
Pablo Álamo <pabloalamovargas@gmail.com>
2022-02-28 17:44:17 +0100

    Ya funciona2

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, List, Todo
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -11,8 +11,9 @@ def setup_admin(app):
     # admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
     
-    # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(List, db.session))
+    admin.add_view(ModelView(Todo, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
